fa1b206c1581e3a572ab7b84cb2260bc30097395
Vipul Singh <114462081+CodesRocky@users.noreply.github.com>
2022-10-27 16:18:15 +0530

    adding auto image slider and some css parts

--- src/component/home_page/Topimg.js
+++ src/component/home_page/Topimg.js
@@ -1,56 +1,19 @@
-import React, { useState } from 'react'
-import {FaArrowLeft,FaArrowRight} from 'react-icons/fa';
-
+import React from 'react'
 import './img.css'
-import img1 from './Npic.jpeg'
-import img2 from './N1pic.jpeg'
-import img3 from './N2pic.jpeg'
-
-
-
 const Topimg = () => {
-    const[pic,setPic]=useState(img1)
-    const mngbtn = () =>
-    {
-        if(pic===img1)
-        {
-        setPic(img2)
-        }
-        else if(pic===img2)
-        {
-            setPic(img3)
-        }
-        else
-        {
-            setPic(img1)
-        }
-    }
-    const mngbtn2 = () =>
-    {
-        if(pic===img1)
-        {
-        setPic(img3)
-        }
-        else if(pic===img2)
-        {
-            setPic(img1)
-        }
-        else
-        {
-            setPic(img2)
-        }
-    }
-  return (
-    <div className='top_img'>
-        
-        <img src={pic} alt='hotel_pic'/>
-        
-        <div className='scond'>
-            <button className='btn1' onClick={mngbtn}><FaArrowLeft style={{color: 'white',fontSize: '45px'}}/></button>
-            <button className='btn2' onClick={mngbtn2}><FaArrowRight style={{color: 'white',fontSize: '45px'}}/></button>
+
+    return (
+        <div className='top_img'>
+
+            <div className='img_slide'>
+
+            </div>
+
+            <div className='scond'>
+            VIP Resort
+            </div>
         </div>
-    </div>
-  )
+    )
 }
 
 
--- src/home/Home.js
+++ src/home/Home.js
@@ -13,6 +13,7 @@ const Home = () => {
   return (
     <>
       <Topimg />
+      {/* <Imgslider/> */}
       <Location />
       <About />
       <Offers />
